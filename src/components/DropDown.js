549a322d9e22717274eb97ada06f415f4a106785
Jeremy <jeremy.phelippeau.91@gmail.com>
2022-11-09 15:41:48 +0100

    page about

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,23 +1,48 @@
 import React, { useState } from "react";
 import "../styles/about.css";
 
-const DropDown = () => {
+const DropDown = ({ props, title }) => {
   const [isDown, setIsDown] = useState(false);
 
   const handleClick = () => {
     isDown ? setIsDown(false) : setIsDown(true);
   };
 
+  const displayProps = () => {
+    if (typeof props === "object") {
+      return (
+        <ul>
+          {props.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      );
+    } else {
+      return <p>{props}</p>;
+    }
+  };
+
   return (
     <div className="dropdown">
       <div className="dropdown-head">
-        <h3>Fiabilité</h3>
-        <button type="button">
-          <i class="fa-solid fa-chevron-up"></i>
+        <h3>{title}</h3>
+        <button type="button" onClick={() => handleClick()}>
+          {isDown ? (
+            <i class="fa-solid fa-chevron-up"></i>
+          ) : (
+            <i class="fa-solid fa-chevron-down"></i>
+          )}
         </button>
       </div>
-
-      <div className="dropdown-content">A qda q ae a</div>
+      {isDown && (
+        <div
+          className={
+            isDown ? "dropdown-content dropdown-active" : "dropdown-content"
+          }
+        >
+          {displayProps()}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,10 +4,20 @@ import DropDown from "../components/DropDown";
 import "../styles/about.css";
 
 const About = () => {
+  const data = [
+    "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos ainsi que les descriptions sont parfaitement conformes aux logements correspondant.",
+    "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    "Nos équipes se tiennent à votre entière disposition pour vous fournir une expérience parfaite et répondre à l'ensemble de vos questions.",
+    "Kasa peut être tenu responsable en vertu des dispositions légales applicables en cas de faute intentionnelle et grave commise par nous, nos représentants légaux, administrateurs ou mandataires. ",
+  ];
+
   return (
     <div className="main">
       <BannerAbout />
-      <DropDown />
+      <DropDown props={data[0]} title={"fiabilité"} />
+      <DropDown props={data[1]} title={"respect"} />
+      <DropDown props={data[2]} title={"service"} />
+      <DropDown props={data[3]} title={"responsabilité"} />
     </div>
   );
 };
